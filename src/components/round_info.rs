ead7747f2f79c7b8cc842476328d19f87f43497f
Passeriform <bhardwajutkarsh.ub@gmail.com>
2024-09-07 03:34:04 +0530

    [RoundInfo] Update to use text box widget for alignment

--- src/components/round_info.rs
+++ src/components/round_info.rs
@@ -1,8 +1,11 @@
-use ratatui::{buffer::Buffer, layout::{Constraint, Flex, Layout, Margin, Rect}, style::{Color, Stylize}, text::Line, widgets::{Block, BorderType, StatefulWidget, Widget}};
+use ratatui::{buffer::Buffer, layout::{Constraint, Flex, Layout, Margin, Rect}, style::{Color, Stylize}, text::Line, widgets::{StatefulWidget, Widget}};
 
-use crate::{core::round::Round, tui::{center_widget, render_centered_lines, render_centered_text}};
+use crate::{core::round::Round, tui::get_line_with_chips};
 
-use super::{blind_badge::BlindBadgeWidget, chip::ChipWidget};
+use super::{blind_badge::BlindBadgeWidget, text_box::TextBoxWidget};
+
+const ROUND_INFO_CONTENT_HEIGHT: u16 = 9;
+const KERNING_MULTIPLIER: u16 = 2;
 
 #[derive(Debug, Default, Clone, Copy)]
 pub struct RoundInfoWidget { }
@@ -14,45 +17,35 @@ impl RoundInfoWidget {
     }
 }
 
+// TODO: Add pub(crate) qualifications
+// TODO: Replace constraints Length with Max and Fill with Min.
+
 impl StatefulWidget for RoundInfoWidget {
     type State = Round;
 
     #[inline]
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let [blind_badge_outer_area, round_info_outer_area] = Layout::horizontal([
-            Constraint::Fill(5),
-            Constraint::Fill(7),
-        ]).areas(area);
+        // Prepare variables
+        let round_info_content = [
+            Line::from("Score at least").centered(),
+            // TODO: Consider using BigText here
+            get_line_with_chips(state.properties.blind.target.to_string(), Color::Red).centered(),
+            // TODO: Use reward field from round here.
+            Line::from(vec!["Reward: ".into(), "$$$$".yellow().bold()]).centered(),
+        ];
 
-        let blind_badge_inner_area = center_widget(
-            blind_badge_outer_area,
-            Constraint::Length(15),
-            Constraint::Length(9),
-        );
-        let blind_badge_inner_area = blind_badge_inner_area.inner(&Margin::new(1, 1));
-
-        let round_info_outer_area = round_info_outer_area.inner(&Margin::new(1, 1));
-        let round_info_outer_area = center_widget(
-            round_info_outer_area.inner(&Margin::new(1, 1)),
+        // Prepare areas
+        let [inner_area] = Layout::vertical([
+            Constraint::Length(ROUND_INFO_CONTENT_HEIGHT),
+        ]).flex(Flex::Center).areas(area);
+        let [blind_badge_area, round_info_area] = Layout::horizontal([
+            Constraint::Length(ROUND_INFO_CONTENT_HEIGHT * KERNING_MULTIPLIER),
             Constraint::Fill(1),
-            Constraint::Length(11),
-        );
-        let round_info_inner_area = round_info_outer_area.inner(&Margin::new(1, 1));
-        let [score_pretext_area, score_area, reward_area] = Layout::vertical([Constraint::Fill(1); 3]).areas(round_info_inner_area);
-        let [chip_area, score_text_area] = Layout::horizontal([Constraint::Fill(1); 2]).flex(Flex::Center).areas(score_area);
+        ]).flex(Flex::SpaceAround).areas(inner_area);
 
-        let reward_line: Line = vec![
-            "Reward: ".into(),
-            "$$$".yellow().bold(),
-        ].into();
-
-        BlindBadgeWidget::new().render(blind_badge_inner_area, buf, &mut state.properties.blind);
-        Block::bordered().border_type(BorderType::Rounded).render(round_info_outer_area, buf);
-        render_centered_text("Score at least", score_pretext_area, buf);
-        // TODO: Add ChipWidget to chip_image_area
-        ChipWidget::new(Color::Red).render(chip_area, buf);
-        // TODO: Make score text cover 3 lines (BigText)
-        render_centered_text(state.properties.blind.target.to_string(), score_text_area, buf);
-        render_centered_lines(vec![reward_line], reward_area, buf);
+        // Render widgets
+        BlindBadgeWidget::new().render(blind_badge_area.inner(Margin::new(2, 1)), buf, &mut state.properties.blind);
+        // TODO: Move to TextBox that will also display border
+        TextBoxWidget::bordered(round_info_content).flex(Flex::SpaceAround).render(round_info_area, buf);
     }
 }
