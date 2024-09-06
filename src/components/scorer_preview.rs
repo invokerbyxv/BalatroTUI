b42b85cf23d3c95a04e09797262b089a0e951411
Passeriform <bhardwajutkarsh.ub@gmail.com>
2024-09-07 03:38:25 +0530

    [ScorePreview] Migrate score preview to use text box widget

--- src/components/scorer_preview.rs
+++ src/components/scorer_preview.rs
@@ -1,6 +1,12 @@
-use ratatui::{buffer::Buffer, layout::{Constraint, Layout, Margin, Rect}, widgets::{Block, BorderType, StatefulWidget, Widget}};
+use ratatui::{buffer::Buffer, layout::{Constraint, Flex, Layout, Rect}, text::Line, widgets::{StatefulWidget, Widget}};
 
-use crate::{core::{card::Card, scorer::{Scorer, ScoringHand}}, tui::render_centered_text};
+use crate::core::{card::Card, scorer::{Scorer, ScoringHand}};
+
+use super::text_box::TextBoxWidget;
+
+const SCORER_PREVIEW_CONTENT_HEIGHT: u16 = 8;
+
+// TODO: Use nested area constraints for min-maxing.
 
 #[derive(Debug, Default, Clone, Copy)]
 pub struct ScorerPreviewWidget { }
@@ -12,36 +18,39 @@ impl ScorerPreviewWidget {
     }
 }
 
+// TODO: Add const modifier to struct creation methods
+
 impl StatefulWidget for ScorerPreviewWidget {
     type State = Vec<Card>;
 
     #[inline]
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let [area] = Layout::vertical([Constraint::Length(12)]).areas(area);
-        let [scoring_hand_area, scoring_area] = Layout::vertical([Constraint::Fill(1); 2]).areas(area.inner(&Margin::new(1, 1)));
-        let [chips_area, multiply_sign_area, multiplier_area] = Layout::horizontal([
-            Constraint::Fill(1),
-            Constraint::Length(1),
-            Constraint::Fill(1),
-        ]).areas(scoring_area);
-
-        let scoring_hand = Scorer::get_scoring_hand(state.clone()).unwrap();
+        // Prepare variables
+        // TODO: Pass these from outside or implement caching to avoid needless calls.
+        let (scoring_hand, _) = Scorer::get_scoring_hand(&state).unwrap();
         let (chips, multiplier) = Scorer::get_chips_and_multiplier(scoring_hand).unwrap();
 
+        // Prepare areas
+        let [inner_area] = Layout::vertical([Constraint::Length(SCORER_PREVIEW_CONTENT_HEIGHT)]).flex(Flex::Center).areas(area);
+        let [scoring_hand_area, scoring_area] = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Length(5),
+        ]).flex(Flex::Center).areas(inner_area);
+        let [chips_area, multiply_sign_area, multiplier_area] = Layout::horizontal([
+            Constraint::Fill(1),
+            Constraint::Length(3),
+            Constraint::Fill(1)
+        ]).areas(scoring_area);
 
-        // TODO: Use big text to render chips, multiplier, scoring hand and multiply icon.
-        Block::bordered().border_type(BorderType::Rounded).render(area, buf);
-
+        // Render widgets
         if scoring_hand != ScoringHand::None {
-            render_centered_text(format!("{} [lvl. {}]", scoring_hand, 1), scoring_hand_area, buf);
+            // TODO: Add leveling system for scoring hand types
+            TextBoxWidget::new([Line::from(format!("{} [lvl. {}]", scoring_hand, 1)).centered()]).render(scoring_hand_area, buf);
         }
+        // TODO: Use big text to render chips, multiplier, scoring hand and multiply icon.
+        TextBoxWidget::bordered([Line::from(chips.to_string()).centered()]).render(chips_area, buf);
+        TextBoxWidget::new([Line::from("×".to_string()).centered()]).render(multiply_sign_area, buf);
+        TextBoxWidget::bordered([Line::from(multiplier.to_string()).centered()]).render(multiplier_area, buf);
 
-        Block::bordered().border_type(BorderType::Rounded).render(chips_area, buf);
-        render_centered_text(format!("{}", chips), chips_area, buf);
-
-        render_centered_text("×", multiply_sign_area, buf);
-
-        Block::bordered().border_type(BorderType::Rounded).render(multiplier_area, buf);
-        render_centered_text(format!("{}", multiplier), multiplier_area, buf);
     }
 }
