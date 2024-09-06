882954ba216d3c2c5beee74fbc7d237b38a0d8b0
Passeriform <bhardwajutkarsh.ub@gmail.com>
2024-09-07 03:28:42 +0530

    [TUI] Add panic hooks and failover mechanisms

--- src/tui.rs
+++ src/tui.rs
@@ -1,10 +1,12 @@
+use better_panic::{Settings, Verbosity};
 use crossterm::{
     cursor,
     event::{DisableMouseCapture, EnableMouseCapture},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use ratatui::{backend::CrosstermBackend as Backend, layout::{Constraint, Flex, Layout, Rect}, Frame, Terminal};
-use std::{error::Error, io::{stderr, Stderr}, ops::{Deref, DerefMut}};
+use ratatui::{backend::CrosstermBackend as Backend, layout::{Constraint, Flex, Layout, Rect}, style::{Color, Style, Styled}, text::{Line, Span, Text}, Frame, Terminal};
+use std::{error::Error, io::{stderr, Stderr}, ops::{Deref, DerefMut}, panic::set_hook, process::exit};
+use color_eyre::{config::HookBuilder, Result as EyreResult};
 
 use crate::event::Event;
 
@@ -22,6 +24,7 @@ impl Tui {
     #[inline]
     pub fn enter(&self) -> Result<(), Box<dyn Error>> {
         enable_raw_mode()?;
+        init_panic_hook()?;
         crossterm::execute!(
             stderr(),
             EnterAlternateScreen,
@@ -59,7 +62,7 @@ impl Tui {
 }
 
 impl Deref for Tui {
-    type Target = ratatui::Terminal<Backend<Stderr>>;
+    type Target = Terminal<Backend<Stderr>>;
 
     #[inline]
     fn deref(&self) -> &Self::Target {
@@ -82,7 +85,7 @@ impl Drop for Tui {
 }
 
 pub trait TuiComponent {
-    fn draw(&self, frame: &mut Frame, rect: Rect);
+    fn draw(&mut self, frame: &mut Frame, rect: Rect);
     fn handle_events(&mut self, _event: Event) { }
 }
 
@@ -92,4 +95,55 @@ pub fn center_widget(area: Rect, horizontal: Constraint, vertical: Constraint) -
         .areas(area);
     let [area] = Layout::vertical([vertical]).flex(Flex::Center).areas(area);
     area
+}
+fn init_panic_hook() -> EyreResult<()> {
+    let (panic_hook, eyre_hook) = HookBuilder::default()
+        .panic_section(format!("This is a bug. Consider reporting it at {}", env!("CARGO_PKG_REPOSITORY")))
+        .display_location_section(true)
+        .display_env_section(true)
+        .into_hooks();
+
+    eyre_hook.install()?;
+
+    set_hook(Box::new(move |panic_info| {
+        if let Ok(t) = Tui::new() {
+            if let Err(r) = t.exit() {
+                error!("Unable to exit Terminal: {:?}", r);
+            }
+        }
+
+        let msg = format!("{}", panic_hook.panic_report(panic_info));
+
+        #[cfg(not(debug_assertions))]
+        {
+            eprintln!("{}", msg); // prints color-eyre stack trace to stderr
+            use human_panic::{handle_dump, print_msg, Metadata};
+            let meta = Metadata {
+                version: env!("CARGO_PKG_VERSION").into(),
+                name: env!("CARGO_PKG_NAME").into(),
+                authors: env!("CARGO_PKG_AUTHORS").replace(':', ", ").into(),
+                homepage: env!("CARGO_PKG_HOMEPAGE").into(),
+            };
+
+            let file_path = handle_dump(&meta, panic_info);
+            // prints human-panic message
+            print_msg(file_path, &meta).expect("human-panic: printing error message to console failed");
+        }
+
+        error!("Error: {}", strip_ansi_escapes::strip_str(msg));
+
+        #[cfg(debug_assertions)]
+        {
+            // Better Panic stacktrace that is only enabled when debugging.
+            Settings::auto()
+                .most_recent_first(false)
+                .lineno_suffix(true)
+                .verbosity(Verbosity::Full)
+                .create_panic_handler()(panic_info);
+        }
+
+        exit(libc::EXIT_FAILURE);
+    }));
+
+    Ok(())
 }
