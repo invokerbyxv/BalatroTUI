dff16df7a14617ba852c094839c61d6bc90712ba
Passeriform <bhardwajutkarsh.ub@gmail.com>
2024-09-07 03:37:42 +0530

    [RunStats] Migrate run stats to use text box widget

--- src/components/run_stats.rs
+++ src/components/run_stats.rs
@@ -1,6 +1,10 @@
-use ratatui::{buffer::Buffer, layout::{Alignment, Constraint, Layout, Rect}, widgets::{Block, BorderType, StatefulWidget, Widget}};
+use ratatui::{buffer::Buffer, layout::{Constraint, Flex, Layout, Margin, Rect}, text::Line, widgets::{StatefulWidget, Widget}};
 
-use crate::{core::run::Run, tui::render_centered_text};
+use crate::core::run::Run;
+
+use super::text_box::TextBoxWidget;
+
+const RUN_STATS_CONTENT_HEIGHT: u16 = 15;
 
 #[derive(Debug, Default, Clone, Copy)]
 pub struct RunStatsWidget { }
@@ -12,27 +16,29 @@ impl RunStatsWidget {
     }
 }
 
+// TODO: Use Layout with flex wherever possible.
+
 impl StatefulWidget for RunStatsWidget {
     type State = Run;
 
     #[inline]
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
-        let [round_functional_info_area, money_area, round_meta_info_area] = Layout::vertical([Constraint::Length(5); 3]).areas(area);
-        let [hands_area, discards_area] = Layout::horizontal([Constraint::Ratio(1, 2); 2]).areas(round_functional_info_area);
-        let [ante_area, round_number_area] = Layout::horizontal([Constraint::Ratio(1, 2); 2]).areas(round_meta_info_area);
+        // Prepare areas
+        let [inner_area] = Layout::vertical([Constraint::Length(RUN_STATS_CONTENT_HEIGHT)]).flex(Flex::Center).areas(area);
+        let [run_info_button_area, run_stats_area] = Layout::horizontal([
+            Constraint::Fill(1),
+            Constraint::Fill(2),
+        ]).areas(inner_area);
+        let [round_functional_info_area, money_area, round_meta_info_area] = Layout::vertical([Constraint::Max(5); 3]).flex(Flex::Center).areas(run_stats_area.inner(Margin::new(1, 1)));
+        let [hands_area, discards_area] = Layout::horizontal([Constraint::Fill(1); 2]).areas(round_functional_info_area);
+        let [ante_area, round_number_area] = Layout::horizontal([Constraint::Fill(1); 2]).areas(round_meta_info_area);
 
-        let stat_block = Block::bordered().border_type(BorderType::Rounded).title_alignment(Alignment::Center);
-
-        stat_block.clone().title("Hands").render(hands_area, buf);
-        stat_block.clone().title("Discards").render(discards_area, buf);
-        stat_block.clone().title("Money").render(money_area, buf);
-        stat_block.clone().title("Ante").render(ante_area, buf);
-        stat_block.clone().title("Round").render(round_number_area, buf);
-
-        render_centered_text(state.round.properties.hands.to_string(), hands_area, buf);
-        render_centered_text(state.round.properties.discards.to_string(), discards_area, buf);
-        render_centered_text(format!("{}$", state.money), money_area, buf);
-        render_centered_text(state.properties.ante.to_string(), ante_area, buf);
-        render_centered_text(state.round.properties.round_number.to_string(), round_number_area, buf);
+        // Render widgets
+        // TODO: Load RunInfoButtonWidget here.
+        TextBoxWidget::bordered([Line::from(state.round.properties.hands.to_string()).centered()]).title("Hands").render(hands_area, buf);
+        TextBoxWidget::bordered([Line::from(state.round.properties.discards.to_string()).centered()]).title("Discards").render(discards_area, buf);
+        TextBoxWidget::bordered([Line::from(format!("{}$", state.money)).centered()]).title("Money").render(money_area, buf);
+        TextBoxWidget::bordered([Line::from(state.properties.ante.to_string()).centered()]).title("Ante").render(ante_area, buf);
+        TextBoxWidget::bordered([Line::from(state.round.properties.round_number.to_string()).centered()]).title("Round").render(round_number_area, buf);
     }
 }
